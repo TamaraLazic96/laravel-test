df3a99ec4115bf0a4cf928ba30fefc710e6e9389
Tamara Lazic <t.lazic@tlazic-n.levi9.com>
2020-11-06 16:28:55 +0100

    Added bade template for single post; added model, migration and controller for post - and single post view method

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('/admin', 'AdminController@index')->name('admin.index');
+
+Route::get('/post', 'PostController@show')->name('post');
